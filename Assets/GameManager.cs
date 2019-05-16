275701bd8366f3cb43b9abb6751df8830d78b45f
xiens <xiens2@gmail.com>
2019-05-16 18:18:17 +0200

    Added script for falling branches in new branch

---
+++ Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
+    public int points = 0;
+    public int healths = 3;
+                                                         
+    void Awake()
+    {
+        if (instance == null)
+        instance = this;
+
+        else if (instance != this)
+        Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/FallingBranches/SpawnBranch.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBranch : MonoBehaviour
+{
+    public GameObject Branch;
+    public float Width = 10.0f;
+    public float SpawnTime = 0.5f;
+
+    bool IsPlayerUnderBranches = false;
+    Vector3 StartingPos = new Vector3(0,0,0);
+    float StartRange, EndRange;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartingPos = transform.position - new Vector3(Width/2,0,0);
+        StartRange = transform.position.x - Width / 2;
+        EndRange =   transform.position.x + Width / 2;
+        StartCoroutine(BranchSpawner());
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            IsPlayerUnderBranches = true;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            IsPlayerUnderBranches = false;
+        }
+    }
+
+    public IEnumerator BranchSpawner()
+    {
+        bool spawn = true;
+
+        while (spawn)
+        {
+            StartingPos = new Vector3(Random.Range(StartRange, EndRange), StartingPos.y, StartingPos.z);
+            if (IsPlayerUnderBranches)  Instantiate(Branch, StartingPos, Quaternion.identity);
+            yield return new WaitForSeconds(SpawnTime);
+        }
+
+    }
+
+}
