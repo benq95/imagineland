e25dcaaef67234374dc4e9b63262a41aee86df28
Rdulemba <rdulemba@gmail.com>
2019-06-02 16:51:18 +0200

    Underwater scene created

---
+++ Assets/Scripts/PlayerUnderwaterScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUnderwaterScript : MonoBehaviour
+{
+
+    private Rigidbody2D rigidbody2D;
+    private Vector2 directionVector;
+    [SerializeField] private GameObject direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Rotate(0, 0, 1);
+        } else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Rotate(0, 0, -1);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            directionVector = direction.transform.position - this.transform.position;
+            directionVector.Normalize();
+            rigidbody2D.AddForce(directionVector);
+        } else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            directionVector = this.transform.position - direction.transform.position;
+            directionVector.Normalize();
+            rigidbody2D.AddForce(directionVector);
+        }
+
+    }
+    
+
+}
