52306d2df0b05901cb1b98af946f10ceb97633e0
Rdulemba <rdulemba@gmail.com>
2019-06-04 12:32:09 +0200

    Story data and story scripts created

---
+++ Assets/StoryData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "StoryData", menuName = "ScriptableObjects/StoryDataObject", order = 1)]
+public class StoryData : ScriptableObject
+{
+    public String description;
+    public Sprite photo;
+    public StoryData nextStoryData;
+}
